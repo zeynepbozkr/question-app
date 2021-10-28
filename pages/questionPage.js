f1d9a4c16e8ec300d2475bd12c2e5f2247fa08fa
zeynepbozkr <73405507+zeynepbozkr@users.noreply.github.com>
2021-10-28 14:58:23 +0300

    change finish button

--- pages/questionPage.js
+++ pages/questionPage.js
@@ -1,27 +1,20 @@
 import React, { useState, useEffect } from "react";
 import { questionData } from "../questions";
-import { Button, Card, Row, Col, message, Radio } from "antd";
+import { Button, Card, Drawer, Row, Col, message, Radio } from "antd";
 import "antd/dist/antd.css";
 import styles from "../styles/Question.module.css";
 
 function questionPage({ questions }) {
   const [count, setCount] = useState(0);
-  const [values, setValues] = useState(0);
+  const [values, setValues] = useState([]);
   const [trueValue, setTrueValue] = useState(0);
   const [fifty, setFifty] = useState(false);
   const [fiftyValue, setFiftyValue] = useState(null);
   const [disable, setDisable] = useState(false);
   const [disable2, setDisable2] = useState(false);
-
   const [showAnswer, setShowAnswer] = useState(false);
-
   const [visible, setVisible] = useState(false);
-  const showDrawer = () => {
-    setVisible(true);
-  };
-  const onClose = () => {
-    setVisible(false);
-  };
+  const [lastValue, setLastValue] = useState();
 
   useEffect(() => {
     //TODO : Bu alanda(useeeffect de) ben (bekir) yardım ettim. Musa abi haberin olsun. :)
@@ -36,6 +29,17 @@ function questionPage({ questions }) {
     const randomV = newA[Math.floor(Math.random() * newA.length)];
     setFiftyValue(randomV);
   }, [count]);
+
+  const showDrawer = () => {
+    setVisible(true);
+    if (values === questions[count].trueAnswer) {
+      setTrueValue(trueValue + 1);
+    }
+  };
+
+  const onClose = () => {
+    setVisible(false);
+  };
 
   const backClick = () => {
     if (count !== 0) {
@@ -54,33 +58,39 @@ function questionPage({ questions }) {
     setDisable(true);
   };
   const nextClick = () => {
-    if (count !== questions.length) {
-      setCount(count + 1);
-    } else if (count === 2) {
+    console.log(questions.length - 2, "questions.length");
+    if (count === questions.length - 2) {
+      message.info("last question");
     }
+    if (values && count !== questions.length) {
+      setCount(1 + count);
+    }
+
     if (values === questions[count].trueAnswer) {
       setTrueValue(trueValue + 1);
     }
     setFifty(false);
     setShowAnswer(false);
   };
-  const finishClick = () => {
-    message.info(trueValue);
-    console.log("tıklandı");
-  };
+  console.log(count, "count");
+
   const answerChange = (e) => {
     setValues(e.target.value);
+    if (count === questions.length - 2) {
+      setLastValue(e.target.value);
+    }
   };
-  console.log(values);
+
+  console.log(values, "vvvv");
   console.log(trueValue, "true");
 
   return (
     <div className={styles.container}>
       <Button onClick={backClick}> BACK</Button>
 
-      <Card className={styles.card} extra={count + 1}>
-        {questions[count].question}
-        <br />
+      <Card className={styles.card}>
+        {count + 1}
+        <h3>{questions[count].question}</h3> <br />
         <Radio.Group onChange={answerChange}>
           <br />
 
@@ -110,8 +120,12 @@ function questionPage({ questions }) {
       <Button onClick={answerClick} disabled={disable2}>
         trueShow
       </Button>
-
-      <Button onClick={finishClick}>finish</Button>
+      <Button type="primary" onClick={showDrawer}>
+        finish
+      </Button>
+      <Drawer placement="top" onClose={onClose} visible={visible}>
+        <h1>correct numbers: {trueValue} </h1>
+      </Drawer>
     </div>
   );
 }
